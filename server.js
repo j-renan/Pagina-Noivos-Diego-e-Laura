82ba11f021e862b50d2c670b8267b61ea16af6ab
j-renan <desen.renan@gmail.com>
2022-05-30 14:14:28 -0300

    Add server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const appName = 'laura-diego';
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + `/dist/${appName}`));
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname, `dist/${appName}/index.html`)
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado  na porta ${PORT}`);
+})
